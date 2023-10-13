29ab744d23b82dca8cb664027cdc29f922668b19
jcorreaw <juninhocorrea2014@gmail.com>
2023-10-13 15:53:10 -0300

    Aula 04

--- src/main/java/br/com/jcorreaw/toDoList/task/ITaskRepository.java
+++ src/main/java/br/com/jcorreaw/toDoList/task/ITaskRepository.java
@@ -2,7 +2,11 @@ package br.com.jcorreaw.toDoList.task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+    List<TaskModel> findByIdUser(UUID idUser);
+
 }
